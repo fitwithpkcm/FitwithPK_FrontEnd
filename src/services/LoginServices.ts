d4a3ec3386fba7be35353b047b5d1a7779459dd2
basil1112 <contact.basil1112@gmail.com>
2025-07-03 16:54:56 +0530

    login servies param added

--- src/services/LoginServices.ts
+++ src/services/LoginServices.ts
@@ -1,7 +1,7 @@
-import { httpCall,httpUpload} from "./HttpService";
+import { httpCall, httpUpload } from "./HttpService";
 import { API_URL } from "../common/Urls";
 
-export const login = params => {
+/* export const login = params => {
     return httpCall({
         url: API_URL.LOGIN,
         method: "post",
@@ -9,57 +9,67 @@ export const login = params => {
     }).then(response => {
         return response;
     });
+}; */
+
+
+export const login = (params: unknown = null) => {
+    return httpCall({
+        url: API_URL.LOGIN,
+        method: "post",
+        data: params
+    }).then((response: unknown) => {
+        return response;
+    });
 };
 
-export const validateToken = params => {
+export const validateToken = (params: unknown) => {
     return httpCall({
         url: API_URL.VALIDATE_TOKEN_USERTYPE,
         method: "post",
         data: params
-    }).then(response => {
+    }).then((response: unknown) => {
         return response;
     });
 };
 
-
-export const registerUser = params => {
+export const registerUser = (params: unknown) => {
     return httpCall({
         url: API_URL.REGISTRATION,
         method: "post",
         data: params
-    }).then(response => {
+    }).then((response: unknown) => {
         return response;
     });
 };
 
-
-export const onBoardProfileAttributeUpdates = params => {
+export const onBoardProfileAttributeUpdates = (params: unknown) => {
     return httpCall({
         url: API_URL.ONBOARD_PROFILE_ATTRIBUTE_UPDATE,
         method: "post",
         data: params
-    }).then(response => {
+    }).then((response: unknown) => {
         return response;
     });
 };
 
-export const onBoardFileUpload = params => {
+// Keep FormData here as it's a specific known type
+export const onBoardFileUpload = (params: FormData) => {
     return httpUpload({
         url: API_URL.ONBOARD_FILE_UPLOAD,
         method: "post",
         data: params
-    }).then(response => {
+    }).then((response: unknown) => {
         return response;
     });
 };
 
-export const fetchOnBoardUserAttributes = params => {
+// null is fine for default value; still keep param as `unknown`
+export const fetchOnBoardUserAttributes = (params: unknown = null) => {
     return httpCall({
         url: API_URL.ONBOARD_PROFILE_ATTRIBUTE_UPDATE,
         method: "get",
         data: params
-    }).then(response => {
+    }).then((response: unknown) => {
         return response;
     });
 };
-
